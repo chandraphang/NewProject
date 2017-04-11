ce5294e9b0650697b7f74a3f647b01a1d3c7687b
Chandra <chandra@ukirama.com>
2017-04-11 13:57:38 +0700

    Add username link to send email
    lalala

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,8 @@ ActiveRecord::Schema.define(version: 20170410074628) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name",  null: false
+    t.string "email", null: false
   end
 
   add_foreign_key "blogs", "users"
